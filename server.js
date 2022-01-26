86a9ed1fa78e8acf9ee1c6cdb86c9df21a580689
mandudu <35313236+mandudu@users.noreply.github.com>
2022-01-25 22:47:04 -0500

    asdf

--- server.js
+++ server.js
@@ -1,24 +1,41 @@
 // Require http module
-
+var http = require('http')
 // Require fs module
+const fs = require('fs')
 
 // Require minimist module (make sure you install this one via npm).
+
+var minimist = require('minimist')
+
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 
+const args = require('minimist')(process.argv.slice(2))
+args['port']
+// args['--port=']
+// console.log('--port=')
 // Define allowed argument name 'port'.
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-
+var port = normalizePort(process.env.PORT || '3000')
+app.set('port', port)
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
+
+fs.readFileSync('./www/index.html', 'utf8',(err,data) => {
+        if(err){
+            console.error(err)
+            return
+            process.exitCode(1)
+        }
+        console.log(data)
+    })
+
 // The function must read a file located at `./www/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
 
 // If there is an error, put it on the console error, return, and exit with error code 1. 
 // Do not be nice about exiting.
-
-
 
 
 
@@ -29,14 +46,17 @@
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./www/index.html.
 
-
-
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(data)
+})
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
-
-
+server.listen(port)
+console.log('Server listening on port ${port}')
 
 // That's it! You're all done!
